6a241dc6850b15facd6aa2c01a3874bdee474531
girlgenius <64441142+girlgenius@users.noreply.github.com>
2020-06-25 02:38:23 -0700

    Add existing website

---
+++ script.js
@@ -0,0 +1,77 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+(function ($) {
+	var controller = new ScrollMagic.Controller();
+
+	/*------------------------------------*\
+        $NUMBERS
+    \*------------------------------------*/
+
+	function animateNumber() {
+		$(".js-count").each(function () {
+			var $this = $(this),
+				countTo = $this.attr("data-count");
+			$({ countNum: $this.text() }).animate(
+				{
+					countNum: countTo
+				},
+				{
+					duration: 3000,
+					easing: "linear",
+					step: function () {
+						// Count up with commas
+						$this.text(Math.floor(this.countNum).toLocaleString("en"));
+					},
+					complete: function () {
+						// Add comma after done counting
+						$this.text(this.countNum.toLocaleString("en"));
+					}
+				}
+			);
+		});
+	}
+
+	new ScrollMagic.Scene({
+		triggerElement: ".section"
+	})
+		.on("enter", function () {
+			$(".section").addClass("pod--animated");
+			animateNumber();
+		})
+		.on("leave", function () {
+			$(".section").removeClass("pod--animated");
+		})
+		.addTo(controller);
+})(jQuery);
+
+//this is not working so i just commented it out 
+  $(".counter").countUp();
+   var _gaq = _gaq || [];
+       _gaq.push(["_setAccount", "UA-36251023-1"]);
+       _gaq.push(["_setDomainName", "jqueryscript.net"]);
+       _gaq.push(["_trackPageview"]);
+
+       (function() {
+         var ga = document.createElement("script");
+         ga.type = "text/javascript";
+         ga.async = true;
+         ga.src =
+           ("https:" == document.location.protocol
+             ? "https://ssl"
+             : "http://www") + ".google-analytics.com/ga.js";
+        var s = document.getElementsByTagName("script")[0];
+         s.parentNode.insertBefore(ga, s);
+       })();
